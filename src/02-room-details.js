64b90e6e77c557f22f0cc59143dba3a09ced842d
Lakisha J <lakishajohnson@pursuit.org>
2023-01-29 11:35:53 -0500

    Room details functioning

--- src/02-room-details.js
+++ src/02-room-details.js
@@ -25,7 +25,35 @@ const exampleRoomData = require("../data/rooms");
  *  getRoomByDinosaurName(dinosaurs, rooms, "Pterodactyl");
  *  //> "Dinosaur with name 'Pterodactyl' cannot be found."
  */
-function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {}
+function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
+  //VARIABLES FOR ERROR MESSAGE AND WHATEVER ROOM THE DINOSAUR WILL BE IN
+let errorMessage = `Dinosaur with name '${dinosaurName}' cannot be found.`;
+let dinoRoom = "";
+
+//LOOP THRU DINOSAURS ARRAY
+for (const dinosaur of dinosaurs) {
+  //IF DINOSAURS NAME ARE EQUAL, (37)DINO ROOM VARIABLE CHANGES TO THE DINOSAUR ID
+  if (dinosaur.name === dinosaurName) {
+    dinoRoom = dinosaur.dinosaurId
+  };
+};
+//IF NAMES ARE NOT EQUAL, then it checks for an empty string ("") as well as an undefined value
+if (dinoRoom === "") {
+  //IF EITHER EXISTS, RETURN ERROR MESSAGE
+  return errorMessage
+};
+//LOOP THRU ROOMS ARRAY
+for (let j = 0; j < rooms.length; j++) {
+//LOOP THRU THE OTHER dinosaurs ARRAY. THIS DINOSAUR ARRAY IS INSIDE ROOMS ARRAY: K INSIDE J
+  for (let k = 0; k < rooms[j].dinosaurs.length; k++) {
+  //IF THE DINOSAUR ID STORED IN DINO ROOM IS EQUAL TO A DINOSAUR ID INSIDE THE DINOSAURS ARRAY[K] THAT IS INSIDE THE ROOMS ARRAY[J], RETURN THE NAME OF THE ROOM[J]
+if (dinoRoom === rooms[j].dinosaurs[k]) {
+  return rooms[j].name
+};
+  }//ELSE
+};
+return `Dinosaur with name '${dinosaurName}' cannot be found in any rooms.`
+};
 
 /**
  * getConnectedRoomNamesById()
@@ -49,7 +77,42 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {}
       "Kit Hopkins Education Wing"
     ]
  */
-function getConnectedRoomNamesById(rooms, id) {}
+function getConnectedRoomNamesById(rooms, id) {
+  //DEFAULT VARIABLES > [];
+  let connectsId = [];
+  let everyRoom = [];
+  
+//LOOP THRU ROOMS ARRAY FOR GIVEN ID 
+  for (const room of rooms) {
+//IF ID'S ARE EQUAL, (90)PUT THE CONNECTS TO ARRAY INSIDE ONE OF OUR EMPTY ARRAYS
+    if(room.roomId === id) {
+      connectsId = room.connectsTo
+    };
+  };
+  if (connectsId.length === 0) {//IF rooms.connectsTo ARRAY IS EMPTY, RETURN MESSAGE
+    return `Room with ID of '${id}' could not be found.`
+  };
+//THIS IS NOT A NESTED LOOP, THIS IS LOOPING THRU THE CONNECTS TO ARRAY (89)STORED IN CONNECTS ID 
+  for (let k = 0; k < connectsId.length; k++) {
+    let dinosaurID = connectsId[k]
+//THIS IS A NESTED LOOP, GOING THRU ROOMS ARRAY AGAIN matching, connectsId with A roomId
+    for (let t = 0; t < rooms.length; t++) {
+      let room = rooms[t]
+//if AN id from connectsTo array IS EQUAL TO A roomId, 
+      if(dinosaurID === room.roomId) {
+//push all the names of those rooms into OUR OTHER empty array
+      everyRoom.push(room.name)
+      };
+    };
+  };
+  //if items in rooms.connectsTo array is not the same length as ITEMS IN EVERY ROOM array, return message 
+  if (connectsId.length !== everyRoom.length) {
+    return `Room with ID of 'incorrect-id' could not be found.`
+  };
+  //ELSE return THE array with the names OF ROOMS ["Ticket Center", "Entrance Room"]
+  return everyRoom
+};
+
 
 module.exports = {
   getRoomByDinosaurName,
